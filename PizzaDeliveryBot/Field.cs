40cbbdb72ac0260810783d27db50b427feccbff8
0leXis <alexbashur@gmail.com>
2021-07-07 19:53:37 +0300

    Добавьте файлы проекта.

---
+++ PizzaDeliveryBot/Field.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaDeliveryBot
+{
+    class Field : IField
+    {
+        public int FieldWidth { get; }
+        public int FieldHeight { get; }
+
+        public List<(int x, int y)> PointsToVisit { get; }
+
+        public Field(int width, int height, List<(int x, int y)> pointsToVisit)
+        {
+            if (width < 0)
+                throw new ArgumentException("width can't be negative");
+            if (height < 0)
+                throw new ArgumentException("height can't be negative");
+            if (pointsToVisit == null)
+                throw new ArgumentNullException("pointsToVisit can't be null");
+
+            FieldWidth = width;
+            FieldHeight = height;
+            PointsToVisit = new List<(int x, int y)>(pointsToVisit);
+        }
+    }
+}
---
+++ PizzaDeliveryBot/IField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaDeliveryBot
+{
+    interface IField
+    {
+        int FieldWidth { get; }
+        int FieldHeight { get; }
+
+        List<(int x, int y)> PointsToVisit { get; }
+    }
+}
