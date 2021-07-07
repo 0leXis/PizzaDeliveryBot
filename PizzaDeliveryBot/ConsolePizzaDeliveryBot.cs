dbc47dd6c6d1a87da5f942ee25df39571212628b
0leXis <alexbashur@gmail.com>
2021-07-07 20:10:19 +0300

    ConsolePizzaDeliveryBot class and interfaces

---
+++ PizzaDeliveryBot/ConsolePizzaDeliveryBot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaDeliveryBot
+{
+    class ConsolePizzaDeliveryBot: IPizzaDeliveryBot
+    {
+        private IPathFinder _pathFinder;
+
+        public IPathFinder PathFinder
+        {
+            get => _pathFinder;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("PathFinder can't be null");
+                _pathFinder = value;
+            }
+        }
+
+        public ConsolePizzaDeliveryBot(IPathFinder pathFinder)
+        {
+            PathFinder = pathFinder;
+        }
+        public void DeliverPizza()
+        {
+            Console.WriteLine(PathFinder.CalculatePath());
+        }
+    }
+}
