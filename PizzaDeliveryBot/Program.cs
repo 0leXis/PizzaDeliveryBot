754499744861421b26f19d5f5919f0fa9c4f90f3
0leXis <alexbashur@gmail.com>
2021-07-08 12:02:37 +0300

    Exception handling and "No delivery points" message

--- PizzaDeliveryBot/Program.cs
+++ PizzaDeliveryBot/Program.cs
@@ -7,9 +7,18 @@ namespace PizzaDeliveryBot
         static void Main(string[] args)
         {
             Console.WriteLine("Write string in the following format: {width}x{height} ({point1x}, {point1y}) ({point2x}, {point2y}) ... ({pointNx}, {pointNy})");
-            IField field = new StringFieldProvider(Console.ReadLine()).GetField();
-            IPizzaDeliveryBot bot = new ConsolePizzaDeliveryBot(new OptimalPathFinder(field));
-            bot.DeliverPizza();
+            IField field;
+            try
+            {
+                field = new StringFieldProvider(Console.ReadLine()).GetField();
+                IPizzaDeliveryBot bot = new ConsolePizzaDeliveryBot(new OptimalPathFinder(field));
+                bot.DeliverPizza();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Error: string has incorrect format. Debug info: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
     }
 }
