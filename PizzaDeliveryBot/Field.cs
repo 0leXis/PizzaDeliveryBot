ee0caa6c227b8a39a250b1d8b8ae2bf030b838ec
0leXis <alexbashur@gmail.com>
2021-07-08 10:20:02 +0300

    Bugfix: Field zero size and points out of bound constraints

--- PizzaDeliveryBot/Field.cs
+++ PizzaDeliveryBot/Field.cs
@@ -13,12 +13,16 @@ namespace PizzaDeliveryBot
 
         public Field(int width, int height, List<Point> pointsToVisit)
         {
-            if (width < 0)
-                throw new ArgumentException("width can't be negative");
-            if (height < 0)
-                throw new ArgumentException("height can't be negative");
+            if (width <= 0)
+                throw new ArgumentException("width can't be negative or zero");
+            if (height <= 0)
+                throw new ArgumentException("height can't be negative or zero");
             if (pointsToVisit == null)
                 throw new ArgumentNullException("pointsToVisit can't be null");
+            foreach(var point in pointsToVisit)
+                if(point.X < 0 || point.X >= width ||
+                   point.Y < 0 || point.Y >= height)
+                    throw new ArgumentException("pointsToVisit contains an out of bounds point");
 
             FieldWidth = width;
             FieldHeight = height;
