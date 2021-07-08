07bc4279c8cbf4394d228ec24f5a96562daf11fb
0leXis <alexbashur@gmail.com>
2021-07-08 10:31:37 +0300

    Bugfix: StringFieldProvider int parse checks

--- PizzaDeliveryBot/StringFieldProvider.cs
+++ PizzaDeliveryBot/StringFieldProvider.cs
@@ -21,15 +21,23 @@ namespace PizzaDeliveryBot
             if (!match.Success || match.Length != InputString.Length)
                 throw new ArgumentException("Wrong string format");
 
-            int width = int.Parse(match.Groups[1].Value);
-            int height = int.Parse(match.Groups[2].Value);
+            if(!int.TryParse(match.Groups[1].Value, out int width))
+                throw new ArgumentException("Width is too big or not a number");
+            if (!int.TryParse(match.Groups[2].Value, out int height))
+                throw new ArgumentException("Height is too big or not a number");
 
             Regex pointPattern = new Regex(@"\(([0-9]+), ([0-9]+)\)");
             // List of x and y components of all points
             var pointsInput = pointPattern.Split(match.Groups[3].Value).Where(x => x != " " && x != "").ToList();
             List<Point> points = new List<Point>();
             for(int i = 0; i < pointsInput.Count; i += 2)
-                points.Add(new Point(int.Parse(pointsInput[i]), int.Parse(pointsInput[i + 1])));
+            {
+                if (!int.TryParse(pointsInput[i], out int pointX))
+                    throw new ArgumentException("X of point " + (i / 2) + " is too big or not a number");
+                if (!int.TryParse(pointsInput[i + 1], out int pointY))
+                    throw new ArgumentException("Y of point " + (i / 2) + " is too big or not a number");
+                points.Add(new Point(pointX, pointY));
+            }
 
             return new Field(width, height, points);
         }
